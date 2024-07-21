6446a78e97ac7b185e1d746ff9c8622fdbcd0e73
Ivan Misustin <ivanmis596@gmail.com>
2024-07-21 16:03:50 +0200

    ur ur ur ur
    fuck this shit...

--- main.c
+++ main.c
@@ -3,299 +3,108 @@
 #include <X11/cursorfont.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <unistd.h>
 #include "util.h"
 
+// function declarations
+void configurerequest(XEvent * e);
+void configurenotify(XEvent * e);
+void destroynotify(XEvent * e);
+void maprequest(XEvent * e);
+void mapnotify(XEvent * e);
+void unmapnotify(XEvent * e);
+void keypress(XEvent * e);
+
+int border_width = 4;
 Display *dpy;
 Window root;
+int root_width;
+int root_height;
+void (*handler[LASTEvent]) (XEvent *) = {
+	[ConfigureRequest] = configurerequest,
+	[ConfigureNotify] = configurenotify,
+	[DestroyNotify] = destroynotify,
+	[MapRequest] = maprequest,
+	[MapNotify] = mapnotify,
+	[UnmapNotify] = unmapnotify,
+	[KeyPress] = keypress,
+};
 Cursor cursor;
 
-typedef struct Client Client;
-struct Client {
-	Window wnd;
-	int x;
-	int y;
-	int w;
-	int h;
-	Client *next;
-	Client *prev;
-};
+// function definitions
+void configurerequest(XEvent * e) {
+	XConfigureRequestEvent *ev = &e->xconfigurerequest;
 
-Client *clients = NULL;
-Client *focused = NULL;
-
-int root_w;
-int root_h;
-
-void spawn(const char *cmd) {
-    if (fork() == 0) {
-        if (dpy) {
-            close(ConnectionNumber(dpy));
-        }
-        setsid();
-        setenv("DISPLAY", getenv("DISPLAY"), 1);
-        execl("/bin/sh", "sh", "-c", cmd, (char *)NULL);
-        fprintf(stderr, "execl failed: %s\n", cmd);
-        exit(EXIT_FAILURE);
-    }
-}
-
-void get_window_dimensions(Display *dpy, Window wnd, int *w, int *h) {
-	XWindowAttributes attrs;
-	XGetWindowAttributes(dpy, wnd, &attrs);
-	*w = attrs.width;
-	*h = attrs.height;
-}
-
-void manage(Window w) {
-	printf("We make manage %lu's ass\n", w);
-	XResizeWindow(dpy, w, root_w, root_h);
-
-	XWindowAttributes attrs;
-	XGetWindowAttributes(dpy, w, &attrs);
-
-	// update client list
-	Client *c = clients;
-	while (c != NULL) {
-		if (c->wnd == w) {
-			c->x = attrs.x;
-			c->y = attrs.y;
-			c->w = attrs.width;
-			c->h = attrs.height;
-			break;
-		}
-		c = c->next;
-	}
-}
-
-void handle_create_notify(XEvent e) {
-	// add the window to the list of clients
-	Client *c = malloc(sizeof(Client));
-	c->wnd = e.xcreatewindow.window;
-	c->next = clients;
-	c->prev = NULL;
-	c->x = e.xcreatewindow.x;
-	c->y = e.xcreatewindow.y;
-	c->w = e.xcreatewindow.width;
-	c->h = e.xcreatewindow.height;
-	clients = c;
-
-	int len = 0;
-	Client *tmp = clients;
-	while (tmp != NULL) {
-		len++;
-		tmp = tmp->next;
-	}
-	printf("There are %d clients\n", len);
-
-	printf("A window (,,>﹏<,,) announces that it has been created\n");
-}
-
-void handle_destroy_notify(XEvent e) {
-	printf("A window (,,>﹏<,,) announces that it has been destroyed\n");
-
-	// remove the window from the list of clients
-	Client *c = clients;
-	while (c != NULL) {
-		if (c->wnd == e.xdestroywindow.window) {
-			if (c->prev != NULL) {
-				c->prev->next = c->next;
-			} else {
-				clients = c->next;
-			}
-			if (c->next != NULL) {
-				c->next->prev = c->prev;
-			}
-			free(c);
-			break;
-		}
-		c = c->next;
-	}
-}
-
-void handle_map_request(XEvent e) {
-	printf("A window (,,>﹏<,,) announces that it wants to be mapped\n");
-	XMapWindow(dpy, e.xmaprequest.window);
-	manage(e.xmaprequest.window);
-}
-
-void handle_map_notify(XEvent e) {
-	XSetInputFocus(dpy, e.xmap.window, RevertToPointerRoot, CurrentTime);
-	focused = clients;
-	printf("A window (,,>﹏<,,) announces that it has been mapped\n");
-}
-
-void handle_unmap_notify(XEvent e) {
-	
-	// focus another window
-	
-	Client *c = clients;
-	while (c != NULL) {
-		if (c->wnd == e.xunmap.window) {
-
-			if (c->prev != NULL) {
-				XSetInputFocus(dpy, c->prev->wnd, RevertToPointerRoot, CurrentTime);
-			} else if (c->next != NULL) {
-				XSetInputFocus(dpy, c->next->wnd, RevertToPointerRoot, CurrentTime);
-			}
-
-			free(c);
-			break;
-		}
-		c = c->next;
-	}
-
-	printf("A window (,,>﹏<,,) announces that it has been unmapped\n");
-}
-
-void handle_configure_request(XEvent e) {
-	printf("A window (,,>﹏<,,) wants to be configured\n");
 	XWindowChanges changes;
-	// TODO MORE STUFF
-	manage(e.xconfigurerequest.window);
+	changes.x = ev->x;
+	changes.y = ev->y;
+	changes.width = ev->width;
+	changes.height = ev->height;
+	changes.border_width = border_width;
+	changes.stack_mode = Above;
+
+	XConfigureWindow(dpy, ev->window, CWX|CWY|CWWidth|CWHeight|CWBorderWidth|CWStackMode, &changes);
 }
 
-void handle_configure_notify(XEvent e) {
-	printf("A window (,,>﹏<,,) announces that it has been configured\n");
+void configurenotify(XEvent * e) {
+	XConfigureEvent *ev = &e->xconfigure;
+	printf("A window %lu has been configured\n", ev->window);
 }
 
-int main(void) {
+void destroynotify(XEvent * e) {
+	XDestroyWindowEvent *ev = &e->xdestroywindow;
+	printf("A window %lu has been destroyed\n", ev->window);
+}
+
+void maprequest(XEvent * e) {
+	XMapRequestEvent *ev = &e->xmaprequest;
+	XMapWindow(dpy, ev->window);
+}
+void mapnotify(XEvent * e) {
+	XMapEvent *ev = &e->xmap;
+	printf("A window %lu has been mapped\n", ev->window);
+}
+
+void unmapnotify(XEvent * e) {
+	XUnmapEvent *ev = &e->xunmap;
+	printf("A window %lu has been unmapped\n", ev->window);
+}
+void keypress(XEvent * e) {
+	XKeyPressedEvent *ev = &e->xkey;
+	printf("A key %d has been pressed\n", ev->keycode);
+}
+
+void setup() {
 	dpy = XOpenDisplay(NULL);
 	if (dpy == NULL) {
 		panic("Could not open display...");
 	}
+
 	root = DefaultRootWindow(dpy);
+	root_width = DisplayWidth(dpy, 0);
+	root_height = DisplayHeight(dpy, 0);
 
-	// fetch some info
-	get_window_dimensions(dpy, root, &root_w, &root_h);
+	XSelectInput(dpy, root, SubstructureRedirectMask|SubstructureNotifyMask);
+	XSync(dpy, False);
 
-	// now we register for SubstructureRedirect and SubstructureNotify events
-	int err = XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask);
-	if (err == BadWindow) {
-		panic("Bad window...");
-	}
-	// sync the changes...
-	XSync(dpy, false);
-
-	// mouse and keyboard stuff...
-	//
-	// create the cursor
 	cursor = XCreateFontCursor(dpy, XC_X_cursor);
-	// define the cursor
 	XDefineCursor(dpy, root, cursor);
-	// sync the changes
-	XSync(dpy, false);
-	//
-	// register for mouse events...
-	// XGrabButton(dpy, AnyButton, None, root, false, ButtonPressMask, GrabModeAsync, GrabModeAsync, None, cursor);
-	// sync the changes
-	// XSync(dpy, false);
-	//
-	// register for key events...
-	// XGrabKey(dpy, AnyKey, None, root, false, GrabModeSync, GrabModeAsync);
-	// sync the changes
-	// XSync(dpy, false);
-	//
-	KeyCode enter = XKeysymToKeycode(dpy, XStringToKeysym("Return"));
-	XGrabKey(dpy, enter, Mod4Mask, root, false, GrabModeAsync, GrabModeAsync);
-	KeyCode key_c = XKeysymToKeycode(dpy, XStringToKeysym("c"));
-	XGrabKey(dpy, key_c, Mod4Mask, root, false, GrabModeAsync, GrabModeAsync);
+	XSync(dpy, False);
+}
 
-	KeyCode key_k = XKeysymToKeycode(dpy, XStringToKeysym("k"));
-	XGrabKey(dpy, key_k, Mod4Mask, root, false, GrabModeAsync, GrabModeAsync);
-	KeyCode key_l = XKeysymToKeycode(dpy, XStringToKeysym("l"));
-	XGrabKey(dpy, key_l, Mod4Mask, root, false, GrabModeAsync, GrabModeAsync);
-	// KeyCode key_q = XKeysymToKeycode(dpy, XStringToKeysym("q"));
-	// XGrabKey(dpy, key_q, Mod4Mask, root, false, GrabModeAsync, GrabModeAsync);
-
-	// The event we are handling...
-	XEvent e;
-	// main loop
+void run() {
+	XEvent ev;
+	XSync(dpy, False);
 	for (;;) {
-		XNextEvent(dpy, &e);
-		switch (e.type) {
-			case CreateNotify:
-				handle_create_notify(e);
-				break;
-
-			case DestroyNotify:
-				handle_destroy_notify(e);
-				break;
-
-			case MapRequest:
-				handle_map_request(e);
-				break;
-
-			case MapNotify:
-				handle_map_notify(e);
-				break;
-
-			case UnmapNotify:
-				handle_unmap_notify(e);
-				break;
-
-			case ConfigureRequest:
-				handle_configure_request(e);
-				break;
-
-			case ConfigureNotify:
-				handle_configure_notify(e);
-				break;
-
-			case ButtonPress:
-				printf("Pressed mouse button %d\n", e.xbutton.button);
-				XAllowEvents(dpy, ReplayPointer, CurrentTime);
-				break;
-
-			case KeyPress:
-				printf("Pressed key %d\n", e.xkey.keycode);
-
-				if (e.xkey.keycode == key_k && (e.xkey.state & Mod4Mask)) {
-					focused = focused->prev;
-					if (focused == NULL) {
-						focused = clients;
-					}
-
-					XSetInputFocus(dpy, focused->wnd, RevertToPointerRoot, CurrentTime);
-				}
-
-				if (e.xkey.keycode == key_l && (e.xkey.state & Mod4Mask)) {
-					focused = focused->next;
-					if (focused == NULL) {
-						focused = clients;
-					}
-
-					XSetInputFocus(dpy, focused->wnd, RevertToPointerRoot, CurrentTime);
-				}
-
-				if (e.xkey.keycode == enter && (e.xkey.state & Mod4Mask)) {
-					printf("Starting alacritty...\n");
-					spawn("alacritty");
-				}
-
-				if (e.xkey.keycode == key_c && (e.xkey.state & Mod4Mask)) {
-					printf("Exitting...\n");
-					XCloseDisplay(dpy);
-					exit(EXIT_SUCCESS);
-				}
-
-				XAllowEvents(dpy, ReplayKeyboard, CurrentTime);
-				break;
-
-			case KeyRelease:
-				printf("Released key %d\n", e.xkey.keycode);
-				XAllowEvents(dpy, ReplayKeyboard, CurrentTime);
-				break;
-
-			default:
-				printf("Unhandled event... %d\n", e.type);
-				break;
+		XNextEvent(dpy, &ev);
+		if (handler[ev.type]) {
+			handler[ev.type](&ev);
 		}
-		// sync the changes...
-		XSync(dpy, false);
 	}
+}
 
-	XCloseDisplay(dpy);
+int main() {
+	setup();
+	run();
 	return EXIT_SUCCESS;
 }
--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Xephyr -ac -br -noreset -screen 800x600 -host-cursor :100 &
+Xephyr -ac -br -noreset -screen 1600x1000 -host-cursor :100 &
 sleep 0.1
 DISPLAY=:100 ./iwm &
-sleep 0.1
-DISPLAY=:100 dolphin &
-DISPLAY=:100 dolphin &
-DISPLAY=:100 dolphin &
-DISPLAY=:100 dolphin &
-DISPLAY=:100 dolphin &
+sleep 0.2
+DISPLAY=:100 alacritty &
+#DISPLAY=:100 alacritty &
+#DISPLAY=:100 alacritty &
+#DISPLAY=:100 alacritty &
+#DISPLAY=:100 alacritty &
